925032549b5dcf4c465700e381c5c847e930433c
iespinoza96 <iespinoza@digis01.com>
2022-11-25 12:13:34 -0600

    TAREA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("DE VERDAD QUIERO UNAS VACACIONES");
+
+TAREA.Saludar();
---
+++ ConsoleApp1/TAREA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TAREA
+    {
+        public static void Saludar()
+        {
+            Console.WriteLine("Que onda");
+            Console.ReadKey();
+        }
+    }
+}
